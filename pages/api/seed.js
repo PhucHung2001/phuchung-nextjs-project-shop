624d4c7f269b1345c45ba911220165f6a139adcb
hungluu.pix <hungluu.pix@gmail.com>
2022-09-08 21:38:18 +0700

    aaazz

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,4 +1,4 @@
-import nc from 'next-connect';
+import nc from "next-connect";
 // import Product from '../../models/Product';
 // import db from '../../utils/db';
 // import data from '../../utils/data';
@@ -7,14 +7,14 @@ import nc from 'next-connect';
 const handler = nc();
 
 handler.get(async (req, res) => {
-  return res.send({ message: 'already seeded' });
-  // await db.connect();
-  // await User.deleteMany();
-  // await User.insertMany(data.users);
-  // await Product.deleteMany();
-  // await Product.insertMany(data.products);
-  // await db.disconnect();
-  // res.send({ message: 'seeded successfully' });
+  // return res.send({ message: 'already seeded' });
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
+  await db.disconnect();
+  res.send({ message: "seeded successfully" });
 });
 
 export default handler;
